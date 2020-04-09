ebd0ed82f270e5c52804482e8cef41ab808a9476
Tim Hardeck <thardeck@suse.com>
2020-04-09 10:33:39 +0200

    Add required go buildpack to dynatrace test

--- src/staticfile/integration/deploy_app_with_dynatrace_test.go
+++ src/staticfile/integration/deploy_app_with_dynatrace_test.go
@@ -21,6 +21,11 @@ var _ = Describe("CF Staticfile Buildpack", func() {
 
 	BeforeEach(func() {
 		dynatraceAPI = cutlass.New(Fixtures("fake_dynatrace_api"))
+		dynatraceAPI.Buildpacks = []string{
+			"https://github.com/suse/cf-go-buildpack#master",
+			"staticfile_buildpack",
+		}
+
 		dynatraceAPI.SetEnv("BP_DEBUG", "true")
 
 		Expect(dynatraceAPI.Push()).To(Succeed())
